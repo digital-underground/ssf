6669c6144d3cdfc22fbdf71fb61b1383a802b3a6
“digital”-underground” <michael.gene.martin@gmail.com>
2024-08-15 20:50:47 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,89 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { StaticImage } from 'gatsby-plugin-image'
+import PropTypes from 'prop-types';
 import './index.css' // Optional: For any custom styles
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Divider from '@mui/material/Divider';
+import Testimonials from '../components/testimonials';
+import getLPTheme from './getLPTheme';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 
-const IndexPage = () => {
+
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
   return (
-    <main>
-      <Header></Header>
-      <Helmet>
-        <link rel="icon" href="/favicon.ico" />
-        <title>Softstack Factory</title>
-        <meta name="description" content="Softstack Factory - Site Under Construction" />
-      </Helmet>
-      <div className="container">
-        <StaticImage
-          src="../images/logo.png" // Update this path to your logo's location
-          alt="Softstack Factory Logo"
-          placeholder="none"
-          layout="fixed"
-          width={420} // Adjust the width as necessary
-        />
-        <p>Our site is currently under construction. Check back soon for updates!</p>
-      </div>
-      <Divider />
-      <Footer></Footer>
-    </main>
-    
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100dvw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color="primary"
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label="Platform"
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+            pointerEvents: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}>Material Design 2</ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  );
 }
 
-export default IndexPage
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+};
+
+
+export default function IndexPage() {
+  const [mode, setMode] = React.useState('light');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
+  return (
+    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+    {/* <CssBaseline /> */}
+    <Header />
+    <Divider />
+    <Box sx={{ bgcolor: 'background.default', marginTop: '464px' }}>
+      <Divider />
+      <Testimonials />
+      <Divider />
+      <Divider />
+      <Footer />
+    </Box>
+  </ThemeProvider>
+);
+}
+
+
